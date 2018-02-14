0e26ae2aa0e7487c84dc2b97075660696be6f195
Step7750 <Step7750@gmail.com>
2018-02-13 18:29:05 -0700

    A3 Skeleton

---
+++ assignment3/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+http.listen(port, () => {
+    console.log('listening on port', port);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+// listen to 'chat' messages
+io.on('connection', function(socket){
+    socket.on('chat', function(msg){
+        io.emit('chat', msg);
+    });
+});
